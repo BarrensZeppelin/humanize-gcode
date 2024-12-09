39599f10ced3f7b96099f5c39d0abf4dccc5efd5
Oskar Haarklou Veileborg <ohv1020@hotmail.com>
2024-12-09 15:20:23 +0100

    Add dependency on `lxml[html_clean]`

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@
 
 from setuptools import setup
 
-with open('README.md', 'r') as readme:
-      long_description = readme.read()
+with open("README.md", "r") as readme:
+    long_description = readme.read()
 
-setup(name='humanize-gcode',
-      version='0.0.5',
-      description='Annotates GCode files with human readable descriptions of commands',
-      author='Oskar Haarklou Veileborg',
-      author_email='ohv1020@hotmail.com',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='https://github.com/BarrensZeppelin/humanize-gcode',
-      python_requires='>=3.6',
-      install_requires=[
-	      'requests_html',
-	      'crayons',
-	      'appdirs',
-      ],
-      packages=['humanize_gcode'],
-      scripts=['scripts/hucode'],
-      classifiers=(
-            'Programming Language :: Python :: 3.6',
-      ))
+setup(
+    name="humanize-gcode",
+    version="0.0.6",
+    description="Annotates GCode files with human readable descriptions of commands",
+    author="Oskar Haarklou Veileborg",
+    author_email="ohv1020@hotmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BarrensZeppelin/humanize-gcode",
+    python_requires=">=3.6",
+    install_requires=[
+        "lxml[html_clean]",
+        "requests_html",
+        "crayons",
+        "appdirs",
+    ],
+    packages=["humanize_gcode"],
+    scripts=["scripts/hucode"],
+    classifiers=("Programming Language :: Python :: 3.6",),
+)
